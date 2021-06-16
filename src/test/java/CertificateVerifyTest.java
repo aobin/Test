9a8c0372326e9d5653a719df292daff88efd7c86
bin ao <bin.ao@valtech.com>
2021-06-16 21:40:42 +0800

    123123

--- src/test/java/CertificateVerifyTest.java
+++ src/test/java/CertificateVerifyTest.java
@@ -2,10 +2,7 @@ import com.aobin.CertificateVerify;
 import org.junit.Test;
 import sun.misc.BASE64Decoder;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -23,9 +20,12 @@ public class CertificateVerifyTest {
     public void validateCertificateChainSuccess() throws IOException, CertificateException, NoSuchAlgorithmException, InvalidKeySpecException {
         final CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
 //        final File file = new File(CertificateVerifyTest.class.getClassLoader().getResource("server.cer").getFile());
-        final File file = new File(CertificateVerifyTest.class.getClassLoader().getResource("server1.cer").getFile());
-        final FileInputStream fileInputStream = new FileInputStream(file);
-        final X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(fileInputStream);
+//        final File file = new File(CertificateVerifyTest.class.getClassLoader().getResource("server1.cer").getFile());
+        String certificateStr = "-----BEGIN CERTIFICATE-----\nMIID1jCCAb4CCQDO0oD3WvNjDDANBgkqhkiG9w0BAQUFADBvMQswCQYDVQQGEwJD\nTjETMBEGA1UECAwKbXlwcm92aW5jZTEPMA0GA1UEBwwGbXljaXR5MRcwFQYDVQQK\nDA5teW9yZ2FuaXphdGlvbjEQMA4GA1UECwwHbXlncm91cDEPMA0GA1UEAwwGbXlu\nYW1lMB4XDTIxMDYxNjEzMTk0M1oXDTIxMDYxNzEzMTk0M1owbzELMAkGA1UEBhMC\nQ04xEzARBgNVBAgMCm15cHJvdmluY2UxDzANBgNVBAcMBm15Y2l0eTEXMBUGA1UE\nCgwObXlvcmdhbml6YXRpb24xEDAOBgNVBAsMB215Z3JvdXAxDzANBgNVBAMMBm15\nbmFtZTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAtG5lgcMrvvMz54velM1n\nZjMMVfwpZITTwYie30ZkvUZSN+SmJ7I5QVjLJclTMOFXxJyHwHMGkBMR9A1B9per\nogR/QRtFVUPVYvQhI7ihmrb90bFrVg34jqJ6Ontaa+okGfgAXmCSBOnYoc0Rd/eJ\nBFy+ymaxIEXYiNbj5Xbz2DUCAwEAATANBgkqhkiG9w0BAQUFAAOCAgEAPkuUfndW\nWwDgndhCZ9ctCHUoek/yu0EnLbAIFbbBMboZvzUTRtcyUePQDFNO6Hjt4SjtR5M4\nq3xG8FTu8+cAR1vTe30qTRuLWx2N/JpiNQk+FqkvZNVLXMKa3obIYPgkKDusmsuf\nV5pl1RsvMZ3BC9fWJtV/B8MHcS+n4LpXp3tbMoig5CSM1XddVB+RE7JkMhrZxkFk\neJnzHlhnMnDHuPcfvYgcWNxy8+rebzlga/GBjO2RI9HAZaZvKMp93rtr95FTRpgg\niYUrsz3hqXrKSfx1idAAgORItyvqXcaX7APhOh8aiVKL0OrjiriXv4PHi6ot33I/\nRAvORAYhraxPa4f92VBgND0T0Fd15iLspvj9YjfvwrjVQFBu1l0bIFvJCvt45YLx\nMuLq+rKZrEyNkQbazupzGxk1DN6Ay5Oueo/jh+95ct75EeFDVrNoW81PFwIc7Kwj\nXJ472AVjwpu0qljlrKro17nhECiAy/xg7sDibXidBaTaQkJDJGbRdUYMxV54gdo+\nAIw3whP/XZObptVdlE+5o138QRRSldSSpoRZKtutI03R81TVWjEESyqwVU0UixUR\ndKB+yVcCD4Bfiwbd/GFCavpO6oUauU96hjw6JaXdGmC7o+dqjgI9Zlgpria/AyVB\nl9VntVPmd0RMjRHLOFcfqql5ZV9DOw31v/4=\n-----END CERTIFICATE-----\n";
+//        final FileInputStream fileInputStream = new FileInputStream(file);
+//        final X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(fileInputStream);
+
+        final X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(certificateStr.getBytes()));
         byte[] encoded = x509Certificate.getPublicKey().getEncoded();
         PublicKey publicKey = getPublicKey();
         try {
